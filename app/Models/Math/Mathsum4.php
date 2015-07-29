7f6da89113bc7bc12c08d3a523d33f61691e929e
cnweibo <matiascx@163.com>
2015-07-29 04:16:30 +0000

    add mathsum models

---
+++ app/Models/Math/Mathsum4.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Kidsit\Models\Math;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Mathsum4 extends Model
+{
+	protected $table = 'mathsum4exercises';
+	protected $guarded = [];
+	public $timestamps=false;
+}
--- app/models/math/Mathsum1.php
+++ app/models/math/Mathsum1.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Mathsum1 extends Model
 {
-    //
+    protected $table = 'mathsum1exercises';
+    protected $guarded = [];
+    public $timestamps=false;
 }
